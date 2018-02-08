bd8db6524ad8ccb413ae1f8fa382474bccb60a28
duartevicentedrumond <duartevicentedrumond@gmail.com>
2018-02-08 16:17:54 +0000

    now statistics section from scp page have a working chart

--- js/file.js
+++ js/file.js
@@ -1,39 +1,56 @@
-alert("está a funcionar")
+console.log(cor);
 
-var ctx = document.getElementById("myChart").getContext('2d');
+Chart.defaults.global.legend.display = false;
+
+var ctx = document.getElementById("ranking");
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: jogos,
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
+            label: '',
+            data: valor_y,
+            backgroundColor: cor,
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255, 162, 153, 1)'
             ],
-            borderWidth: 1
-        }]
+            borderWidth: 0,
+        }],
+        pointRadius: 0,
     },
     options: {
         scales: {
+            xAxes: [{
+                gridLines: {
+                    color: "rgba(0, 0, 0, 0)",
+                },
+                display: false,
+                categoryPercentage: 1.0,
+                barPercentage: 1.0
+            }],
             yAxes: [{
                 ticks: {
-                    beginAtZero:true
+                    beginAtZero: true
+                },
+                display: false,
+                gridLines: {
+                    color: "rgba(0, 0, 0, 0)",
                 }
             }]
-        }
-    }
+        },
+        tooltips: {
+            enabled: true,
+            callbacks: {
+                // use label callback to return the desired label
+                label: function (tooltipItem, data) {
+                    return tooltipItem.xLabel;
+                },
+                title: function (tooltipItem, data) {
+                    return;
+                },
+            },
+            displayColors: false,
+
+        },
+    },
 });
